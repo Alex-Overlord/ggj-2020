15d508759e7c816fe563d637688ff4dd13ff54ad
Alex <canton.alex@live.fr>
2020-02-01 18:03:22 +0100

    unity

---
+++ Unity-Repair/Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour {
+    public float speed = 10f;
+    private Vector2 moveDir = Vector2.zero;
+    private Player player;
+
+    void Start() {
+        player = GetComponent<Player>();
+    }
+
+    void Update() {
+        //move
+    }
+
+    void movePlayer() {
+        if (Input.GetKeyDown(KeyCode.Z)) {
+
+        }
+    }
+}
